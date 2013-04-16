9aab4be287d23dcbc43be56f5cf622595cf10295
Johann-Michael THIEBAUT <johann.thiebaut@gmail.com>
2013-04-16 16:21:09 +0200

    size and concat implemented

--- src/sav/IntList.scala
+++ src/sav/IntList.scala
@@ -2,24 +2,34 @@ import leon.Utils._
 
 object obj {
 
-   abstract class IntList
+  abstract class IntList
 
-   case object Nil extends IntList
+  case object Nil extends IntList
 
-   case class Cons(hd: Int, tl: IntList) extends IntList
+  case class Cons(hd: Int, tl: IntList) extends IntList
 
-   def isEmpty(l: IntList) = l match {
-      case Nil => true
-      case _   => false
-   }
+  def isEmpty(l: IntList) = l match {
+    case Nil => true
+    case _   => false
+  }
 
-   def head(l: IntList) = l match {
-      case Nil        => error("head of emtpy list")
-      case Cons(x, t) => x
-   }
+  def head(l: IntList) = l match {
+    case Nil        => error("head of emtpy list")
+    case Cons(x, t) => x
+  }
 
-   def tail(l: IntList) = l match {
-      case Nil        => error("tail of emtpy list")
-      case Cons(x, t) => t
-   }
+  def tail(l: IntList) = l match {
+    case Nil        => error("tail of emtpy list")
+    case Cons(x, t) => t
+  }
+
+  def size(l: IntList): Int = (l match {
+    case Nil        => 0
+    case Cons(_, t) => 1 + size(t)
+  }) ensuring (size => size >= 0)
+
+  def concat(l1: IntList, l2: IntList): IntList = (l1 match {
+    case Nil        => l2
+    case Cons(x, t) => Cons(x, concat(t, l2))
+  }) ensuring (l => size(l) == size(l1) + size(l2))
 }
